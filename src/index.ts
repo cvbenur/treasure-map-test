2b2e80127cdd7e6c472fd5592065fd86d2a4f218
Ruben Nabet <rubenabet@gmail.com>
2021-12-01 19:14:06 +0100

    feat(adv-display): add adventurer display to printMap()

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { loadDataFromFile } from "./services/file-parser.service";
 import { printMap } from "./utils/map.utils";
 
-const { map, adventurers } = loadDataFromFile(process.cwd() + '/test-maps/test-map-1.txt');
+const { map, adventurers } = loadDataFromFile(process.cwd() + '/test-maps/test-map-2.txt');
 
 if (map !== null) {
-  printMap(map);
+  printMap(map, adventurers);
 }
--- src/utils/map.utils.ts
+++ src/utils/map.utils.ts
@@ -1,6 +1,7 @@
 import { SquareType } from "../enums/square-type.enum"
 import { TreasureMap } from "../models/interfaces/treasure-map.interface";
 import { TreasureSquare, Square } from "../models/interfaces/square.interface";
+import { Adventurer } from "../models/interfaces/adventurer.interface";
 
 /**
  * Returns the appropriate displayable string from the {@link SquareType}
@@ -30,11 +31,27 @@ export function getSquareSymbol(square: Square): string {
   return symbol;
 }
 
+function getAdventurerSymbol(adv: Adventurer): string {
+  return `A(${adv.name})`;
+}
+
 /**
  * Displays the provided {@link TreasureMap} (used for debugging purposes)
  * @param map The map to display
  */
-export function printMap(map: TreasureMap) {
-  // TODO: Adventurers
-  console.log(map.layout.map(row => row.map(getSquareSymbol).join('\t')).join('\n'));
+export function printMap(map: TreasureMap, adventurers?: Adventurer[]) {
+  // Derive printable map from layout
+  const toPrint = map.layout.map(row => row.map(getSquareSymbol));
+
+  // If there are any adventurers to display
+  if (adventurers) {
+    // Iterate over provided Adventurer array
+    for (const adventurer of adventurers) {
+      // Replace square by Adventurer found at this location
+      toPrint[adventurer.loc.y][adventurer.loc.x] = getAdventurerSymbol(adventurer);
+    }
+  }
+
+  // Print the result
+  console.log(toPrint.map(row => row.join('\t')).join('\n'));
 }
