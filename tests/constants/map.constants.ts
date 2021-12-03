c0d5193c1a64fd97c37c23cf15fba3a37f3c72db
Ruben Nabet <rubenabet@gmail.com>
2021-12-03 20:39:34 +0100

    chore(map-utils): remove dead code and related tests

---
+++ tests/constants/map.constants.ts
@@ -0,0 +1,6 @@
+import { TreasureMap } from "../../src/models/interfaces/treasure-map.interface";
+import { loadDataFromFile } from "../../src/services/file-parser.service";
+import { INPUT_DIRECTORY_PATH } from "../../src/constants/file.constants";
+import { join } from "path";
+
+export const MAP_2_DATA = loadDataFromFile(join(INPUT_DIRECTORY_PATH, 'test-map-2.txt'));
--- tests/utils/map.utils.test.ts
+++ tests/utils/map.utils.test.ts
@@ -1,46 +1,20 @@
-import { SquareType } from "../../src/enums/square-type.enum";
-import { Square, MountainSquare, TreasureSquare } from "../../src/models/interfaces/square.interface";
-import { getSquareSymbol } from "../../src/utils/map.utils";
+import { getMapAsFormattedText } from "../../src/utils/map.utils";
+import {
+  MAP_2_DATA
+} from "../constants/map.constants";
+import { TreasureMap } from "../../src/models/interfaces/treasure-map.interface";
+
 
 describe('map.utils.ts', () => {
-  describe('getSquareSymbol() - used for debugging', () => {
-    it('should return the correct symbol for an empty square', () => {
-      const emptySquare: Square = {
-        loc: { x: 1, y: 1 },
-        type: SquareType.NORMAL,
-        canStop: false,
-      };
-
-      const actual = getSquareSymbol(emptySquare);
-      const expected = '.';
-
-      expect(actual).toBe(expected);
-    });
-
-    it('should return the correct symbol for a mountain square', () => {
-      const mtnSquare: MountainSquare = {
-        loc: { x: 1, y: 1 },
-        type: SquareType.MOUNTAIN,
-        canStop: true,
-      };
-
-      const actual = getSquareSymbol(mtnSquare);
-      const expected = 'M';
-
-      expect(actual).toBe(expected);
-    });
-
-    it('should return the correct symbol for a treasure square', () => {
-      const trsSquare: TreasureSquare = {
-        loc: { x: 1, y: 1 },
-        type: SquareType.TREASURE,
-        canStop: false,
-        treasures: 4,
-      };
-
-      const actual = getSquareSymbol(trsSquare);
-      const expected = 'T(4)';
-
+  describe('getMapAsFormattedText()', () => {
+    it('should return correctly formatted map data', () => {
+      const actual = getMapAsFormattedText(MAP_2_DATA.map as TreasureMap);
+      const expected = 'C - 3 - 4\n'
+        + 'M - 1 - 0\n'
+        + 'M - 2 - 1\n'
+        + 'T - 0 - 3 - 2\n'
+        + 'T - 1 - 3 - 3\n';
+      
       expect(actual).toBe(expected);
     });
   });
