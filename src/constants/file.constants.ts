87a7acb4ba726ad32b19bb3b0a325f7e83933bb6
Ruben Nabet <rubenabet@gmail.com>
2021-12-04 13:25:53 +0100

    docs(readme): write instructions into README

--- src/constants/file.constants.ts
+++ src/constants/file.constants.ts
@@ -1,6 +1,9 @@
 import { join } from 'path';
 
-export const INPUT_DIRECTORY_PATH = join(process.cwd(), 'test-maps');
-export const OUTPUT_DIRECTORY_PATH = join(process.cwd(), 'generated');
+const INPUT_DIRECTORY = 'test-maps';
+const OUTPUT_DIRECTORY = 'generated';
+
+export const INPUT_DIRECTORY_PATH = join(process.cwd(), INPUT_DIRECTORY);
+export const OUTPUT_DIRECTORY_PATH = join(process.cwd(), OUTPUT_DIRECTORY);
 
 export const FILENAME = 'test-map-2.txt';
